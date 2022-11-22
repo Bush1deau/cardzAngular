12e9d98986bcf8ba8a2e58bc9ac6bdee1584b6d9
Evyweb <dev@evyweb.fr>
2022-11-23 00:19:17 +0100

    feat(tp): Added the basic stuff to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SocialMedia} from "./social-media";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cardz';
+
+  // Envoi de données à la vue
+  userCard = {
+    name: 'Kirito Kirigaya',
+    avatar: 'assets/avatar.jpg',
+    backgroundImage: 'assets/background.jpg',
+    biography: "Epéiste, Kirito est un personnage de fiction de l'univers de Sword Art Online.",
+    links: [
+      {title: 'Visitez mon Github pour des projets sympas!',     icon: SocialMedia.GITHUB,    url: 'https://www.youtube.com/watch?v=iik25wqIuFo'},
+      {title: 'Faites un tour sur ma chaîne Youtube',    icon: SocialMedia.YOUTUBE,   url: 'https://www.youtube.com/watch?v=iik25wqIuFo'},
+      {title: 'Ne manquez pas une info sur Twitter',    icon: SocialMedia.TWITTER,   url: 'https://www.youtube.com/watch?v=iik25wqIuFo'},
+      {title: 'Pour les contacts pro rdv sur LinkedIn',   icon: SocialMedia.LINKEDIN,  url: 'https://www.youtube.com/watch?v=iik25wqIuFo'},
+      {title: 'Pour me voir en bikini sur Instagram',  icon: SocialMedia.INSTAGRAM, url: 'https://www.youtube.com/watch?v=iik25wqIuFo'},
+      {title: 'Jamais utilisé Behance, ça se mange?',    icon: SocialMedia.BEHANCE,   url: 'https://www.youtube.com/watch?v=iik25wqIuFo'},
+      {title: 'Messenger pour les vieux...',  icon: SocialMedia.MESSENGER, url: 'https://www.youtube.com/watch?v=iik25wqIuFo'},
+      {title: 'Ou joignez moi tout simplement par Email',      icon: SocialMedia.EMAIL,     url: 'https://www.youtube.com/watch?v=iik25wqIuFo'},
+    ],
+    animations: {
+      card: 'animate__fadeInDown',
+      backgroundImage: 'animate__fadeIn',
+    }
+  };
+
 }
