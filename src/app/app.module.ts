d7607ae1255ebd991e49dac4bf2542badb97f440
Evyweb <dev@evyweb.fr>
2023-01-03 15:19:10 +0100

    chore(tp): Add reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import {NgModule} from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
@@ -12,7 +13,8 @@ import {LinkComponent} from './link/link.component';
     LinkComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
